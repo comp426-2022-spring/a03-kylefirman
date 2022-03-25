af8af783166e3b64cb50b1e3b435e82ac2517b4b
kylefirman <kpfirman@live.unc.edu>
2022-03-25 11:25:30 -0400

    fixed minimist parsing args

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ import { coinFlip,coinFlips,countFlips,flipACoin } from './coin.mjs'
 import minimist from 'minimist';
 import express from 'express';
 
-const args = minimist(process.argv.slice(2))
+const args = require("minimist")(process.argv.slice(2))
 args['port']
 const port = args.port || process.env.port || 5000
 
