8e8848aa976dcb64cf31cdf15007874b54cec99c
kylefirman <kpfirman@live.unc.edu>
2022-04-01 12:36:45 -0400

    changed position of methods

--- server.js
+++ server.js
@@ -17,35 +17,6 @@ const server = app.listen(PortID, () => {
     console.log('App listening on port %PORT%'.replace('%PORT%', PortID))
 });
 
-// Default response for any other request
-app.use(function(req, res){
-    res.status(404).send('404 NOT FOUND')
-})
-
-app.get('/app/', (req, res) => {
-    // Respond with status 200
-        res.status(200).end("200 OK")
-});
-
-app.get('/app/flip/', (req, res) => {
-    var result = coinFlip();
-    res.status(200).json({ 'flip' : result})
-});
-
-// Work to do, req.params.number references the number variable
-app.get('/app/flips/:number', (req, res) => {
-	const flips = coinFlips(req.params.number)
-	res.status(200).json({"raw" : flips, "summary" : countFlips(flips)})
-});
-
-app.get('/app/flip/call/tails', (req, res) => {
-    res.status(200).json({ 'result' : flipACoin('tails')})
-})
-
-app.get('/app/flip/call/heads', (req, res) => {
-    res.status(200).json({ 'result' : flipACoin('heads')})
-})
-
 //Coin functions
 function coinFlip() {
     let flipped = Math.floor(Math.random() * 2)
@@ -87,6 +58,44 @@ function flipACoin(call) {
     }
     return flipGame;
 }
+
+
+
+
+
+
+
+
+
+// Default response for any other request
+app.use(function(req, res){
+    res.status(404).send('404 NOT FOUND')
+})
+
+app.get('/app/', (req, res) => {
+    // Respond with status 200
+        res.status(200).end("200 OK")
+});
+
+app.get('/app/flip/', (req, res) => {
+    var result = coinFlip();
+    res.status(200).json({ 'flip' : result})
+});
+
+app.get('/app/flips/:number', (req, res) => {
+	const flips = coinFlips(req.params.number)
+	res.status(200).json({"raw" : flips, "summary" : countFlips(flips)})
+});
+
+app.get('/app/flip/call/tails', (req, res) => {
+    res.status(200).json({ 'result' : flipACoin('tails')})
+})
+
+app.get('/app/flip/call/heads', (req, res) => {
+    res.status(200).json({ 'result' : flipACoin('heads')})
+})
+
+
 
 
 
