e7d546b7d3034d0849270015013dbf5802972af1
kylefirman <kpfirman@live.unc.edu>
2022-04-08 18:42:58 -0400

    test

--- server.js
+++ server.js
@@ -1,101 +1,81 @@
-// Importing Functions
-//import { coinFlip,coinFlips,countFlips,flipACoin } from './coin.mjs'
-import minimist from 'minimist';
-import express from 'express';
-
-const minimist = require('minimist')
-const args = minimist(process.argv.slice(2))
-args["port"]
-const PortID = args.port || process.env.port || 5000
-
-// Require Express.js
 const express = require('express')
 const app = express()
+const args = require('minimist')(process.argv.slice(2))
 
-// Start an app server
-const server = app.listen(PortID, () => {
-    console.log('App listening on port %PORT%'.replace('%PORT%', PortID))
+args["port"]
+
+const port = args.port || process.env.PORT || 5000;
+
+const server = app.listen(port, () => {
+    console.log('App listening on port %PORT%'.replace('%PORT%', port))
 });
-
-//Coin functions
-function coinFlip() {
-    let flipped = Math.floor(Math.random() * 2)
-    if (flipped == 0) {
-      return "heads";
-    } else {
-      return "tails";
-    }
-}
-
-function coinFlips(flips) {
-    let results = [flips];
-    for (let i = 0; i < flips; i++) {
-      results[i] = coinFlip();
-    }
-    return results;
-}
-
-function countFlips(array) {
-    let headcount = 0;
-    let tailscount = 0;
-    for (let i = 0; i < array.length; i++) {
-      if (array[i] == "heads") {
-        headcount++;
-      } else {
-        tailscount++;
-      }
-    }
-    let FlipResults = ["heads " + headcount, "tails " + tailscount];
-    return FlipResults;
-}
-
-function flipACoin(call) {
-    let actual = coinFlip();
-    if (call.equals(actual)) {
-      let flipGame = ["call: " + call, "flip: " + actual, "result: win"];
-    } else {
-      let flipGame = ["call: " + call, "flip: " + actual, "result: lose"];
-    }
-    return flipGame;
-}
-
-
-
-
-
-
-
-
-
-// Default response for any other request
-app.use(function(req, res){
-    res.status(404).send('404 NOT FOUND')
-})
 
 app.get('/app/', (req, res) => {
-    // Respond with status 200
-        res.status(200).end("200 OK")
+      res.statusCode = 200;
+      res.statusMessage = 'OK';
+      res.end(res.statusCode+ ' ' +res.statusMessage);
+      res.type("text/plain");
 });
 
+function coinFlip() {
+    if (Math.random() >= 0.5) {
+      return "heads"
+    } else {
+      return "tails"
+    }
+}
+  
+function coinFlips(flips) {
+    const values = []
+    for (let i = 0; i < flips; i++) {
+      values[i] = coinFlip()
+    }
+    return values
+}
+  
+function countFlips(array) {
+    let count = { heads: 0, tails: 0 }
+  array.forEach(element => 
+    { if (element == "heads") {
+      count.heads += 1
+    } else {
+      count.tails += 1
+    }
+   })
+   return count
+}
+  
+function flipACoin(call) {
+    let coin = { call: call, flip: "NULL", result: "NULL"}
+    let flip = coinFlip()
+    coin.flip = flip
+    if (call == flip) {
+      coin.result = "win"
+    } else {
+      coin.result = "lose"
+    }
+    return coin
+}
+
 app.get('/app/flip/', (req, res) => {
-    var result = coinFlip();
-    res.status(200).json({ 'flip' : result})
+  const flip = coinFlip()
+	res.status(200).json({"flip" : flip})
 });
 
 app.get('/app/flips/:number', (req, res) => {
-	const flips = coinFlips(req.params.number)
-	res.status(200).json({"raw" : flips, "summary" : countFlips(flips)})
+  const flips = coinFlips(req.params.number)
+  const counted = countFlips(flips)
+  res.status(200).json({"raw" : flips, "summary" : counted})
 });
 
-app.get('/app/flip/call/tails', (req, res) => {
-    res.status(200).json({ 'result' : flipACoin('tails')})
-})
+app.get('/app/flip/call/:call', (req, res) => {
+  const called = flipACoin(req.params.call)
+  res.status(200).json({called})
+});
 
-app.get('/app/flip/call/heads', (req, res) => {
-    res.status(200).json({ 'result' : flipACoin('heads')})
-})
-
-
-
-
-
+app.use(function(req, res){
+  res.statusCode = 404;
+      res.statusMessage = 'NOT FOUND';
+      res.end(res.statusCode+ ' ' +res.statusMessage);
+      res.type("text/plain");
+});
