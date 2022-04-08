03b5a0a9c1b6e0bef5d6fd4fa71661b41e15408a
kylefirman <kpfirman@live.unc.edu>
2022-04-08 18:59:39 -0400

    i don t understand

--- server.js
+++ server.js
@@ -4,10 +4,10 @@ const args = require('minimist')(process.argv.slice(2))
 
 args["port"]
 
-const portID = args.port || process.env.PORT || 5000;
+const port = args.port || process.env.PORT || 5000;
 
 const server = app.listen(port, () => {
-    console.log('App listening on port %PORT%'.replace('%PORT%', portID))
+    console.log('App listening on port %PORT%'.replace('%PORT%', port))
 });
 
 app.get('/app/', (req, res) => {
